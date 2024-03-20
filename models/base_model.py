52d8e2e4c3e8b4ec43a630d08a226e4b07473836
Paul 311 <bizimapaul777>
2024-03-20 16:27:36 +0100

    alot of work

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,22 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *arg, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        time_iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, time_iso_format))
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
